1bea42be8519ffa4f2d0861d698d3043226c194b
letdevx <leticiasistemasads@gmail.com>
2022-10-16 01:47:30 -0300

    Esboco inicial do template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,11 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
+from .models import Etapa_Cliente
 
 # Create your views here.
 def index(request):
-    context = {}
+    clientes_etapas = Etapa_Cliente.objects.all()
+    context = {
+        'clientes_etapas': clientes_etapas
+    }
     return render(request, 'dashboard/index.html', context)
