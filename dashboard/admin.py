15491cbeda081d566f1b37fb1743e0e06830de71
letdevx <leticiasistemasads@gmail.com>
2022-10-16 01:33:53 -0300

    Configurado o app admin.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Etapa, Etapa_Cliente, Cliente, Funcionario, Tarefa
+
+admin.site.register(Etapa)
+admin.site.register(Etapa_Cliente)
+admin.site.register(Cliente)
+admin.site.register(Funcionario)
+admin.site.register(Tarefa)
--- dashboard/models.py
+++ dashboard/models.py
@@ -5,18 +5,28 @@ from django.db import models
 # Cliente ou Obra???
 class Cliente(models.Model):
     nome = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nome
 
 class Etapa(models.Model):
     descricao = models.CharField(max_length=200)
+    def __str__(self):
+        return self.descricao
 
 class Etapa_Cliente(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.cliente.nome}-{self.etapa.descricao}"
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=200)
     etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nome
 
 class Tarefa(models.Model):
     descricao = models.CharField(max_length=200)
     funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.descricao
