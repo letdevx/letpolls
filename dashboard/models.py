327368992045781cfec6e58be344eed4a3e54688
letdevx <leticiasistemasads@gmail.com>
2022-10-16 01:15:58 -0300

    Criado o modelo e o esboço do template.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Cliente ou Obra???
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200)
+
+class Etapa(models.Model):
+    descricao = models.CharField(max_length=200)
+
+class Etapa_Cliente(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE)
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=200)
+    etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE)
+
+class Tarefa(models.Model):
+    descricao = models.CharField(max_length=200)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from http.client import HTTPResponse
-from django.shortcuts import render 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('dash index')
+    context = {}
+    return render(request, 'dashboard/index.html', context)
