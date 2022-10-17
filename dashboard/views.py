ba4fc50e58861f06dbc180d45fa8ff214078f1bd
letdevx <leticiasistemasads@gmail.com>
2022-10-17 00:18:59 -0300

    Feito preenchimento das execucoes e adicionado colspan.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,19 +1,24 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
+from django.db.models import Prefetch, Count
 from .models import Execucao, Cliente, Etapa, Funcionario, Tarefa
 
 # Create your views here.
 def index(request):
-    clientes = Cliente.objects.all()
-    etapas = Etapa.objects.order_by('id').all()
-    funcionarios = Funcionario.objects.order_by('etapa_id','id').all()
+    clientes = Cliente.objects\
+        .prefetch_related(Prefetch('execucao_set', to_attr='execucoes'))\
+        .order_by('execucao__tarefa__id').all()
+    etapas = Etapa.objects\
+        .annotate(qtd_tarefas=Count('funcionario__tarefa'))\
+        .order_by('id').all()
+    funcionarios = Funcionario.objects\
+        .annotate(qtd_tarefas=Count('tarefa'))\
+        .order_by('etapa_id','id').all()
     tarefas = Tarefa.objects.order_by('funcionario_id','id').all()
-    execucoes = Execucao.objects.all()
     context = {
         'clientes': clientes,
         'etapas': etapas,
         'funcionarios': funcionarios,
-        'tarefas': tarefas,
-        'execucoes': execucoes
+        'tarefas': tarefas
     }
     return render(request, 'dashboard/index.html', context)
