defd294e462ad982578db916378d2ddb7f93c03e
letdevx <leticiasistemasads@gmail.com>
2022-11-04 02:15:09 -0300

    Adicionados os comentários.

--- teste/views.py
+++ teste/views.py
@@ -13,21 +13,30 @@ def index(request):
         .prefetch_related(Prefetch('tarefa_set', to_attr='tarefas'))\
         .prefetch_related('tarefa_set__funcionario')
     processos = list(query)
+    # Descobre um colspan para cada processo, pegando sempre o maior (max)
     processos_colspans = zip(processos, [max(processo.qtd_tarefas, processo.qtd_funcionarios) for processo in processos])
+
+    # Para cada processo, obtém uma lista de funcionarios não repetidos (à partir das tarefas do processo)
     funcionarios = []
     for processo in processos:
         funcionarios.extend(set([tarefa.funcionario for tarefa in processo.tarefas]))
+
+    # Para cada processo, para cada funcionario do processo,
+    # se o funcionario tem tarefas nesse processo, conta essas tarefas
+    funcionarios_distintos = list(set(funcionarios)) # set elimina os repetidos
     colspans = []
-    funcionarios_distintos = list(set(funcionarios))
     for processo in processos:
         colspans.extend([funcionario.tarefa_set.filter(processo__id=processo.id).count() for funcionario in funcionarios_distintos if funcionario.tarefa_set.filter(processo=processo)])
     funcionarios_colspans = zip(funcionarios, colspans)
+
+    # Para cada processo, para cada tarefa no processo,
+    # obtém as tarefas ordenadas por funcionario
     tarefas = [tarefa for processo in processos for tarefa in sorted(processo.tarefas, key=lambda t: t.funcionario_id)]
 
     context = {
+        'processos_colspans': processos_colspans,
         'funcionarios_colspans': funcionarios_colspans,
-        'tarefas': tarefas,
-        'processos_colspans': processos_colspans
+        'tarefas': tarefas
     }
 
     return render(request, 'teste/index.html', context)
